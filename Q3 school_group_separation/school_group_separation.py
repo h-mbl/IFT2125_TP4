af6a20662e4ef16c168623824492e8959d8b6987
herve.ngisse <125308992+h-mbl@users.noreply.github.com>
2024-04-13 22:39:11 -0400

    algo2

--- Q3 school_group_separation/school_group_separation.py
+++ Q3 school_group_separation/school_group_separation.py
@@ -1,164 +1,149 @@
-#Nom, matricule
-#Nom, matricule
+# Nom, matricule
+# Nom, matricule
 
 import sys
 
-#Fonction pour lire le fichier d'input. Vous ne deviez pas avoir besoin de la modifier.
-#Retourne la liste des noms d'étudiants (students) et la liste des paires qui ne peuvent
-#doivent pas être mis dans le même groupe (pairs)
+
+# Fonction pour lire le fichier d'input. Vous ne deviez pas avoir besoin de la modifier.
+# Retourne la liste des noms d'étudiants (students) et la liste des paires qui ne peuvent
+# doivent pas être mis dans le même groupe (pairs)
 #
-#Function to read the input file. You shouldn't have to modify it.
-#Returns the list of student names (students) and the list of pairs of students that
-#shouldn't be put in the same group (pairs)
+# Function to read the input file. You shouldn't have to modify it.
+# Returns the list of student names (students) and the list of pairs of students that
+# shouldn't be put in the same group (pairs)
 def read(fileName):
     # lecture du fichier
-    fileIn = open(fileName,"r")
+    fileIn = open(fileName, "r")
     linesIn = fileIn.readlines()
     fileIn.close()
 
     nbStudents = int(linesIn[0])
     students = []
-    if(nbStudents != 0):
-        students = [s.strip() for s in linesIn[1:nbStudents+1]]
-    nbPairs = int(linesIn[nbStudents+1])
+    if (nbStudents != 0):
+        students = [s.strip() for s in linesIn[1:nbStudents + 1]]
+    nbPairs = int(linesIn[nbStudents + 1])
     pairs = []
-    if(nbPairs != 0):
-        pairs = [s.strip().split() for s in linesIn[nbStudents+2:nbStudents+nbPairs+2]]
+    if (nbPairs != 0):
+        pairs = [s.strip().split() for s in linesIn[nbStudents + 2:nbStudents + nbPairs + 2]]
 
     return students, pairs
 
 
-#Fonction qui écrit dans le fichier d'output. 
-#le paramètre content est un string
+# Fonction qui écrit dans le fichier d'output.
+# le paramètre content est un string
 #
-#Function that writes in the output file.
-#The content parameter is a string
+# Function that writes in the output file.
+# The content parameter is a string
 def write(fileName, content):
     with open(fileName, "w") as outputFile:
         for ensemble in content:
             ligne = ' '.join(sorted(ensemble))
             outputFile.write(ligne + "\n")
 
-#Fonction principale à compléter.
-#students : liste des noms des étudiants
-#pairs : liste des paires d'étudiants à ne pas grouper ensemble
-# chaque paire est sous format de liste [x, y]
-#Valeur de retour : string contenant la réponse. Si c'est impossible, retourner "impossible"
-#                   Sinon, retourner en un string les deux lignes représentant les
-#                   les deux groupes d'étudants (les étudiants sont séparés par des
-#                   espaces et les deux lignes séparées par un \n)
-#
-#Function to complete
-#students : list of student names
-#pairs : list of pairs of students that shouldn't be grouped together.
-#        each pair is given as a list [x, y]
-#Return value : string with the output. If it is impossible, return "impossible".
-#               otherwise, return in a single string both ouput lines that contain
-#               two groups (students are separated by spaces and the two lines by a \n)
+
+
 def createGroups(students, pairs):
-    a = 0
-    try:
-        students = [int(x) for x in students]
-        pairs = [[int(element) for element in sous_liste] for sous_liste in
-                                     pairs]
-    except: pass
-    students_sets = [[element] for element in students]
+
     tmp_student = set(students)
     pairs = [set(sous_liste) for sous_liste in pairs]
-    def verifierPresence(ensemble):
-        for element in pairs :
-            if len(ensemble.intersection(element)) > 1 :
-                return False
-        return True
 
-    def combinaison(element):
-        combinaison = []
-        combinaison_set = []
-        for i in range(len(element)):
-            a  = 0
-            elementI = element[i]
-            for j in range(i + 1, len(element)):
-                elementJ = element[j]
-                verifier = set([elementI[-1]] + [elementJ[-1]])
-                if verifier not in pairs :
-                    nvEns = elementI + elementJ[len(elementJ) - 1:]
-                    nvEns_set = set(nvEns)
-                    if len(nvEns) > 1 and nvEns_set not in resultat_set:
-                        combinaison.append(nvEns)
-                        combinaison_set.append(nvEns_set)
-            a = 0
-        return combinaison,combinaison_set
-    def combinaisonPremierePartie(element,debut):
-        combinaison = []
-        combinaison_set = []
-        for i in range(len(element)):
-            for j in range(debut, len(b)):
-                elementI = element
-                elementJ = b[j]
-                verifier = set([elementI[-1]] + [elementJ[-1]])
-                if verifier not in pairs:
-                    nvEns = elementI + elementJ[len(elementJ) - 1:]
-                    nvEns_set = set(nvEns)
-                    if len(nvEns) > 1 and nvEns_set not in resultat_set:
-                        combinaison.append(nvEns)
-                        combinaison_set.append(nvEns_set)
-        return combinaison, combinaison_set
-    def recherche():
-        for i in range(len(resultat_set) - 1):
-            for j in range(i + 1, len(resultat_set)):
-                if resultat_set[i].union(resultat_set[j]) == tmp_student and len(
-                        resultat_set[i].intersection(resultat_set[j])) == 0:
-                    return [resultat_set[i], resultat_set[j]]
-        return "impossible"
+    ensemble1 = set()
+    ensemble2 = set()
 
-    resultat = []
-    resultat_set = []
-    b = students_sets.copy()
-    for z in range(len(b)):
-        a = b[z]
-        tmp, tmp_set = combinaisonPremierePartie(a,z+1)
-        temporaire = tmp.copy()
-        resultat.extend(tmp)
-        resultat_set.extend(tmp_set)
-        if len(temporaire) > 0 :
-            while len(temporaire[0]) < len(students_sets) - 1:
-                tmp_x = []
-                x = temporaire[0]
-                delete = temporaire.copy()
-                for element in delete :
-                    y = element
-                    if x[:-1] == y[:-1] :
-                        tmp_x.append(element)
-                        temporaire.remove(element)
-                    else : break
-                a = 0
-                tmp,tmp_set = combinaison(tmp_x)
-                temporaire.extend(tmp)
-                resultat.extend(tmp)
-                resultat_set.extend(tmp_set)
-                a = 0
-                if len(temporaire) == 0 :
-                    element_recherche = recherche()
-                    if element_recherche != "impossible" :
-                        a = 0
-                        return element_recherche
-                    break
-        else: break
+    cas = {}
 
-    return [{"impossible"}]
+    def ensemble(element,position):
+        if len(cas[element][0].intersection(ensemble1)) == 0 and position == 1:
+            ensemble1.add(element)
+        elif len(cas[element][0].intersection(ensemble2)) == 0 and position == 2:
+            ensemble2.add(element)
+        else :
+            return 0
+
+    def placerDansTableau(element, position):
+        if position == 1:
+            position = 2
+        else:
+            position = 1
+        for x in element:
+            if cas[x][2] == 0:
+                cas[x][2] = position
+
+    def creationDictionnaire():
+        for element in students:
+            tmp = set()
+            for ensemble in pairs:
+                if len(ensemble.intersection({element})) > 0:
+                    reponse = ensemble - {element}
+                    tmp.add(reponse.pop())
+            # ensemble, visited, ensemble
+            if len(tmp) > 0:
+                cas[element] = [tmp, 0, 0]
+            else :
+                ensemble1.add(element)
+                tmp_student.discard(element)
 
 
-#Normalement, vous ne devriez pas avoir à modifier
-#Normaly, you shouldn't need to modify
+    creationDictionnaire()
+    if len(cas) == 0 : return [{"impossible"}]
+    tmp = students[0]
+    elementParcourir = cas[tmp][0].copy()
+    cas[tmp][2] = 1
+    cas[tmp][1] = 1
+    tmp_student.discard(tmp)
+
+
+    ensemble(tmp,1)
+
+    placerDansTableau(elementParcourir,1)
+
+    while len(elementParcourir) > 0 or len(tmp_student) > 0:
+        if len(elementParcourir) > 0 :
+            tmp = elementParcourir.pop()
+            cas[tmp][1] = 1
+            tmp_student.discard(tmp)
+            retour = ensemble(tmp,cas[tmp][2])
+            if retour != None : return [{"impossible"}]
+            tmp_elementParcourir = cas[tmp][0].copy()
+            delete = tmp_elementParcourir.copy()
+            for element in delete:
+                if element not in tmp_student:
+                    tmp_elementParcourir.discard(element)
+            placerDansTableau(tmp_elementParcourir,cas[tmp][2])
+            elementParcourir = elementParcourir.union(tmp_elementParcourir)
+        elif len(tmp_student) > 0:
+            tmp = tmp_student.pop()
+            cas[tmp][1] = 1
+            if cas[tmp][2] == 0:
+                cas[tmp][2] = 1
+            retour = ensemble(tmp, cas[tmp][2])
+            if retour != None: return [{"impossible"}]
+            tmp_elementParcourir = cas[tmp][0].copy()
+            delete = tmp_elementParcourir.copy()
+            for element in delete:
+                if element not in tmp_student:
+                    tmp_elementParcourir.discard(element)
+            placerDansTableau(tmp_elementParcourir, cas[tmp][2])
+            elementParcourir = elementParcourir.union(tmp_elementParcourir)
+
+
+    if len(ensemble2.intersection(ensemble1)) > 0 or len(ensemble1.union(ensemble2)) != len(students):
+        return [{"impossible"}]
+    else :
+        return ensemble1, ensemble2
+
+# Normalement, vous ne devriez pas avoir à modifier
+# Normaly, you shouldn't need to modify
 def main(args):
     input_file = args[0]
     output_file = args[1]
     students, pairs = read(input_file)
     output = createGroups(students, pairs)
+    a = 0
     write(output_file, output)
-            
 
-#Ne pas changer
-#Don't change
+
+# Ne pas changer
+# Don't change
 if __name__ == '__main__':
     main(sys.argv[1:])
